6f9d783b5fae67dbf4fdb620e67877ff46bf74ee
LEO <lst0451@hotmail.com>
2018-05-21 18:04:13 -0400

    init commit

---
+++ src/main/java/com/lstmontreal/social/controller/MainController.java
@@ -0,0 +1,72 @@
+package com.lstmontreal.social.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.reactive.function.client.ClientRequest;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.Collections;
+import java.util.Map;
+
+
+@Controller
+public class MainController {
+
+	@Autowired
+	private OAuth2AuthorizedClientService authorizedClientService;
+
+	@GetMapping("/")
+	public String index(Model model, OAuth2AuthenticationToken authentication) {
+		OAuth2AuthorizedClient authorizedClient =
+				this.authorizedClientService.loadAuthorizedClient(
+						authentication.getAuthorizedClientRegistrationId(),
+						authentication.getName());
+		model.addAttribute("userName", authentication.getName());
+		model.addAttribute("clientName", authorizedClient.getClientRegistration().getClientName());
+//		System.out.println(model.asMap().get("userName"));
+//		System.out.println(model.asMap().get("clientName"));
+		return "index";
+	}
+
+	@GetMapping("/userinfo")
+	public String userinfo(Model model, OAuth2AuthenticationToken authentication) {
+		OAuth2AuthorizedClient authorizedClient =
+				this.authorizedClientService.loadAuthorizedClient(
+						authentication.getAuthorizedClientRegistrationId(),
+						authentication.getName());
+		Map userAttributes = Collections.emptyMap();
+		String userInfoEndpointUri = authorizedClient.getClientRegistration()
+			.getProviderDetails().getUserInfoEndpoint().getUri();
+		if (!StringUtils.isEmpty(userInfoEndpointUri)) {	// userInfoEndpointUri is optional for OIDC Clients
+			userAttributes = WebClient.builder()
+				.filter(oauth2Credentials(authorizedClient))
+				.build()
+				.get()
+				.uri(userInfoEndpointUri)
+				.retrieve()
+				.bodyToMono(Map.class)
+				.block();
+		}
+		model.addAttribute("userAttributes", userAttributes);
+		return "userinfo";
+	}
+
+	private ExchangeFilterFunction oauth2Credentials(OAuth2AuthorizedClient authorizedClient) {
+		return ExchangeFilterFunction.ofRequestProcessor(
+			clientRequest -> {
+				ClientRequest authorizedRequest = ClientRequest.from(clientRequest)
+					.header(HttpHeaders.AUTHORIZATION, "Bearer " + authorizedClient.getAccessToken().getTokenValue())
+					.build();
+				return Mono.just(authorizedRequest);
+			});
+	}
+}
